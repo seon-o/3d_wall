4b6bfd61dba196be5caf25334b4671d3d85ed331
seon-o <seon.o.min@gmail.com>
2020-10-23 15:06:10 +0900

    html

---
+++ js/wall3d.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function() {
+    const houseElem = document.querySelector('.house');
+    let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+
+    window.addEventListener('scroll', function(){
+        
+    });
+})();
