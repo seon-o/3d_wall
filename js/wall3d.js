e9c3b01a847f3b36f9dce5b8879b1fc97f708f07
seon-o <seon.o.min@gmail.com>
2020-10-23 19:20:13 +0900

    wall

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,10 +1,35 @@
-"use strict";
-
 (function() {
+    "use strict";
+
+    const stageElem = document.querySelector('.stage');
     const houseElem = document.querySelector('.house');
-    let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    const barElem = document.querySelector('.progress-bar');
+    const mousePos = {x:0, y:0};
+    let maxScrollValue;
+
+    function resizeHandler(){
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+
+
 
     window.addEventListener('scroll', function(){
-        
+        const scrollPer = pageYOffset / maxScrollValue;
+        const zMove = scrollPer * 980 - 490;
+        houseElem.style.transform = `translateZ(${zMove}vw)`;
+
+        barElem.style.width = scrollPer * 100 + '%';
     });
+
+    window.addEventListener('mousemove', function(e){
+        mousePos.x = -1+(e.clientX/ window.innerWidth)*2;
+        mousePos.y = 1-(e.clientY / window.innerHeight)*2;
+        stageElem.style.transform = `rotateX(${mousePos.y 
+        *5}deg) rotateY(${mousePos.x*5}deg)`;
+    });
+
+
+
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
 })();
